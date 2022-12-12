86fae60e98b86d49ac5ff7204939b21e724e3746
obiecity <115046102+obiecity@users.noreply.github.com>
2022-12-12 16:10:43 -0500

    Update customers.py

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -2,15 +2,13 @@ from flask import Blueprint, request, jsonify, make_response
 import json
 from src import db
 
-
 customers = Blueprint('customers', __name__)
 
-# Get all customers from the DB
 @customers.route('/customers', methods=['GET'])
 def get_customers():
     cursor = db.get_db().cursor()
-    cursor.execute('select customerNumber, customerName,\
-        creditLimit from customers')
+    cursor.execute('select buyerID, firstName,\
+        lastName from Buyer')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -21,11 +19,10 @@ def get_customers():
     the_response.mimetype = 'application/json'
     return the_response
 
-# Get customer detail for customer with particular userID
 @customers.route('/customers/<userID>', methods=['GET'])
 def get_customer(userID):
     cursor = db.get_db().cursor()
-    cursor.execute('select * from customers where customerNumber = {0}'.format(userID))
+    cursor.execute('select * from Buyer where buyerID = {0}'.format(userID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -35,3 +32,15 @@ def get_customer(userID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+@customers.route('/customers/new', methods=['POST'])
+def add_buyer():
+    current_app.logger.info(request.form)
+    cursor = db.get_db().cursor()
+    buyerID = request.form['buyerID']
+    firstName = request.form['firstName']
+    lastName = request.form['lastName']
+    query = f'INSERT INTO Buyer(buyerID, firstName, lastName) VALUES(\"{buyerID}\", \"{firstName}\", \"{lastName}"\)'
+    cursor.execute(query)
+    d.get_db().commit()
+    return "Done!"
