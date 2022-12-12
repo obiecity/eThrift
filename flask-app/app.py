fc0f02b9188c115c63e27a1b138976b7aa6cbf6d
obiecity <115046102+obiecity@users.noreply.github.com>
2022-12-12 16:12:03 -0500

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -9,6 +9,13 @@ from src import create_app
 # create the app object
 app = create_app()
 
+app.register_blueprint(customers, url_prefix='/customers')
+app.register_blueprint(products, url_prefix='/products')
+
+@app.route('/hello/<firstName>')
+def hello_guest(firstName):
+    return f'<h1>Hello, {firstName}! Welcome to eThrift, the best option for online thrifting!<h1>'
+
 if __name__ == '__main__':
     # we want to run in debug mode (for hot reloading) 
     # this app will be bound to port 4000. 
