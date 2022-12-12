b073a947f55d254c2bb3983c2df1bea3c93c0c57
obiecity <115046102+obiecity@users.noreply.github.com>
2022-12-12 16:10:09 -0500

    Update products.py

--- flask-app/src/products/products.py
+++ flask-app/src/products/products.py
@@ -2,60 +2,67 @@ from flask import Blueprint, request, jsonify, make_response
 import json
 from src import db
 
-
 products = Blueprint('products', __name__)
 
-# Get all the products from the database
-@products.route('/products', methods=['GET'])
+@products.route('/products')
 def get_products():
-    # get a cursor object from the database
     cursor = db.get_db().cursor()
-
-    # use cursor to query the database for a list of products
-    cursor.execute('select productCode, productName, productVendor from products')
-
-    # grab the column headers from the returned data
+    cursor.execute('select sellerID, name, price, quantity, size, category from Clothes')
     column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
     json_data = []
-
-    # fetch all the data from the cursor
     theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
     for row in theData:
         json_data.append(dict(zip(column_headers, row)))
-
     return jsonify(json_data)
 
-# get the top 5 products from the database
-@products.route('/top5products')
-def get_most_pop_products():
+@products.route('/cheapproducts')
+def get_cheap_products():
     cursor = db.get_db().cursor()
     query = '''
-        SELECT p.productCode, productName, sum(quantityOrdered) as totalOrders
-        FROM products p JOIN orderdetails od on p.productCode = od.productCode
-        GROUP BY p.productCode, productName
-        ORDER BY totalOrders DESC
-        LIMIT 5;
+        SELECT itemID, name, size, category, price
+        FROM Clothes
+        GROUP BY itemID, name
+        ORDER BY price DESC;
+        LIMIT 25;
     '''
     cursor.execute(query)
-       # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
     json_data = []
-
-    # fetch all the data from the cursor
     theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
     for row in theData:
         json_data.append(dict(zip(column_headers, row)))
-
     return jsonify(json_data)
+
+@products.route('/top10products')
+def get_top_10_products():
+    cursor = db.get_db().cursor()
+    query = '''
+        SELECT name, size, quantity, price
+        FROM Clothes c JOIN Seller s on c.sellerID = s.sellerID
+        GROUP BY name, price
+        ORDER BY avgRating DESC
+        LIMIT 10;
+    '''
+    cursor.execute(query)
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+    return jsonify(json_data)
+
+@products.route('/products/list', methods=['POST'])
+def add_buyer():
+    current_app.logger.info(request.form)
+    cursor = db.get_db().cursor()
+    itemID = request.form['itemID']
+    sellerID = request.form['sellerID']
+    name = request.form['name']
+    quantity = request.form['quantity']
+    price = request.form['price']
+    size = request.form['size']
+    category = request.form['category']
+    query = f'INSERT INTO Clothes(itemID, sellerID, name, quantity, price, size, category) VALUES(\"{itemID}\", \"{sellerID}\", \"{name}"\, \"{quantity}\", \"{price}\", \"{size}\", \"{category}\")'
+    cursor.execute(query)
+    d.get_db().commit()
+    return "Done!"
